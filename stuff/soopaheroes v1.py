3121b0955677c312da2c3ba73c4dfebd12e0f6ec
Spirit Payton <98915494+SpiritPayton@users.noreply.github.com>
2023-03-27 09:53:25 +1300

    stu onda

---
+++ stuff/soopaheroes v1.py
@@ -0,0 +1,15 @@
+hero_0 = {
+    "name": "Materdon",
+    "power": "flight",
+    "strength": 5
+}
+
+hero_1 = {
+    "name": "Spirit",
+    "power": "spiritual powers",
+    "strength": 9
+    "speed": 10
+}
+
+if "name" in hero_0:
+    print(f"The superhero's name is {hero_0['name']}")
