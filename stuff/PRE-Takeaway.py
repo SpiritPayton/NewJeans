496e533fe1c9fd874cb8cb1092357ab40fb707c1
Spirit Payton <98915494+SpiritPayton@users.noreply.github.com>
2023-04-22 23:43:26 +1200

    jo yuri

--- stuff/PRE-Takeaway.py
+++ stuff/PRE-Takeaway.py
@@ -1,8 +1,13 @@
-"""Menu Program for the Takeaway Program
+"""Menu Program for the Takeaway Business
 """
 
 import easygui
 
 menu_choice = easygui.buttonbox(f"Ayo welcome to Thee Terrific Takeaways, here's the menu:", "Menu")
 
-# ice spice
+# create a list of combo meals
+combo_meals = ["Combo Value: Beef burger, Fries, Fizzy drink",
+               "Combo Cheezy: Cheeseburger, Fries, Fizzy drink",
+               "Combo Super: Cheeseburger, Large fries, Smoothie"]
+
+print(combo_meals)
